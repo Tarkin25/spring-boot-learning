b86e671f0f7eed3a84a9f331cfd5043b116e57c0
Severin Weigold <severin.weigold@edu.tbz.ch>
2019-08-21 15:21:07 +0200

    reimported project without JPA

--- src/main/java/ch/noseryoung/learning/SpringBootLearningApplication.java
+++ src/main/java/ch/noseryoung/learning/SpringBootLearningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootLearningApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningApplication.class, args);
+	}
 
 }
